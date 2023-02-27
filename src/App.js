b52adf0aa29ed7a4216a556a099a7a94721b7fb9
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2023-02-27 12:52:27 +0100

    to hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import Searchbar from "./Searchbar/Searchbar";
 import { ProgressBar } from "react-loader-spinner";
 import Modal from "./Modal/Modal";
 
-export default class App extends Component {
-  state = {
-    images: [],
-    input: "",
-    pageNumber: 1,
-    status: "idle",
-    error: null,
-    modal: -1,
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [input, setInput] = useState("");
+  const [pageNumber, setPageNumber] = useState(0);
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+  const [modal, setModal] = useState(-1);
+
+  const handleSubmit = (e, input) => {
+    e.preventDefault();
+    input = input.trim();
+    if (input) {
+      setInput(input);
+      setImages([]);
+      setPageNumber(1);
+    }
   };
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.onEsc, false);
-  }
+  const handleButton = (e) => {
+    e.preventDefault();
+    e.target.disabled = true;
+    setTimeout(() => {
+      e.target.disabled = false;
+    }, 2000);
+    setPageNumber((pageNumber) => pageNumber + 1);
+  };
 
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.input !== this.state.input && this.state.input) {
-      this.setState({ status: "pending" });
-      this.fetchImg();
+  const handleImg = (index) => {
+    setModal(index);
+    console.log("modal", modal);
+    console.log("index", index);
+  };
+
+  const onEsc = (e) => {
+    if (e.key === "Escape") {
+      setModal(-1);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", onEsc, false);
+    return () => {
+      document.removeEventListener("keydown", onEsc, false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (input) {
+      setStatus("pending");
+    }
+  }, [input]);
+
+  useEffect(() => {
+    if (pageNumber === 0) {
       return;
     }
-    if (prevState.pageNumber !== this.state.pageNumber) {
-      this.fetchImg();
-    }
-  };
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.onEsc, false);
-  }
-
-  fetchImg = () => {
     setTimeout(() => {
       fetch(
-        `https://pixabay.com/api/?q=${this.state.input}&page=${this.state.pageNumber}&key=24559894-a9126184585885c934fb0d5f1&image_type=photo&orientation=horizontal&per_page=12`
+        `https://pixabay.com/api/?q=${input}&page=${pageNumber}&key=24559894-a9126184585885c934fb0d5f1&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then((res) => {
           return res.json();
@@ -45,81 +72,44 @@ export default class App extends Component {
           if (res.total === 0) {
             return Promise.reject(new Error("Nothing found"));
           }
-          this.setState((prevState) => {
-            return {
-              images: prevState.images.concat(res.hits),
-              status: "resolved",
-            };
-          });
+          setImages((images) => images.concat(res.hits));
+          setStatus("resolved");
         })
         .catch((error) => {
-          this.setState({ error, status: "rejected" });
+          setError(error);
+          setStatus("rejected");
         });
     }, 2000);
-  };
+  }, [pageNumber, input]);
 
-  handleSubmit = (e, input) => {
-    e.preventDefault();
-    if (this.state.input !== input) {
-      this.setState({ input: input, images: [], pageNumber: 1 });
-    }
-  };
-
-  handleButton = (e) => {
-    e.preventDefault();
-    e.target.disabled = true;
-    setTimeout(() => {
-      e.target.disabled = false;
-    }, 2000);
-    this.setState((prevState) => {
-      return { pageNumber: prevState.pageNumber + 1 };
-    });
-  };
-
-  handleImg = (index) => {
-    this.setState({ modal: index });
-  };
-
-  handleModal = () => {
-    this.setState({ modal: -1 });
-  };
-
-  onEsc = (e) => {
-    if (e.key === "Escape") {
-      this.handleModal();
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <Searchbar handleSubmit={this.handleSubmit} />
-        {this.state.status === "resolved" && (
-          <ImageGallery
-            images={this.state.images}
-            handleButton={this.handleButton}
-            handleImg={this.handleImg}
-          />
-        )}
-        {this.state.status === "rejected" && <>{this.state.error.message}</>}
-        {this.state.status === "pending" && (
-          <ProgressBar
-            height="80"
-            width="80"
-            ariaLabel="progress-bar-loading"
-            wrapperStyle={{}}
-            wrapperClass="progress-bar-wrapper"
-            borderColor="#3f51b5"
-            barColor="#3f51b5"
-          />
-        )}
-        {this.state.modal >= 0 && (
-          <Modal
-            img={this.state.images[this.state.modal].largeImageURL}
-            handleModal={this.handleModal}
-          />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar handleSubmit={handleSubmit} />
+      {status === "resolved" && (
+        <ImageGallery
+          images={images}
+          handleButton={handleButton}
+          handleImg={handleImg}
+        />
+      )}
+      {status === "rejected" && <>{error.message}</>}
+      {status === "pending" && (
+        <ProgressBar
+          height="80"
+          width="80"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass="progress-bar-wrapper"
+          borderColor="#3f51b5"
+          barColor="#3f51b5"
+        />
+      )}
+      {modal >= 0 && (
+        <Modal
+          img={images[modal].largeImageURL}
+          handleModal={() => setModal(-1)}
+        />
+      )}
+    </>
+  );
 }
